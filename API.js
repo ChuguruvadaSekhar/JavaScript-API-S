107cc3cc5706385e9685639002b4f7c4db8818d1
ChuguruvadaSekhar <52273650+ChuguruvadaSekhar@users.noreply.github.com>
2020-01-05 16:39:46 +0530

    first commit

---
+++ API.js
@@ -0,0 +1,12 @@
+let request = new XMLHttpRequest();
+request.open("GET","https://jsonplaceholder.typicode.com/users");
+request.send();
+request.onload = () =>{
+    console.log(request);
+    if(request.status === 200){
+        console.log(JSON.parse(request.response));
+
+    }else{
+        console.log(`error ${ request.status} ${request.statusText}`);
+    }
+}
